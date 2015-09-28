aeb47affd2c0ef3148fb72dae1230acbd8b9bc60
coreyjbrown <coreyjbrown@gmail.com>
2015-09-28 15:57:15 -0400

    Add check to see if user is in db

--- Users.js
+++ Users.js
@@ -1,6 +1,6 @@
 var dbString = PropertiesService.getScriptProperties().getProperty('DBSTRING');
 var userColumns = 'username,roles';
-
+var staffListId = PropertiesService.getScriptProperties().getProperty('STAFFLISTID')
 
 
 function getCurrentUser(){
@@ -27,7 +27,7 @@ function validateUser(){
   
   validUser = NVGAS.getSqlRecords(dbString, query)[0] ? NVGAS.getSqlRecords(dbString, query)[0].username : "";
 
-  return validUser ? true : false;
+  return validUser ? true : getStaffListInfo(CURRENTUSER);
 }
 
 
@@ -45,4 +45,54 @@ function getUserRole(user){
   query = 'SELECT roles FROM Users WHERE username = "' + currentUser + '"'
   userRole = NVGAS.getSqlRecords(dbString, query)[0];
   return userRole;
+}
+
+
+
+function getStaffListInfo(username){
+  var test, staffListSs, staffListSheet, staffList, userRecord, userSchool, query, class, name;
+  
+  staffListSs = SpreadsheetApp.openById(staffListId);
+  staffListSheet = staffListSs.getSheetByName('Sheet1');
+  staffList = NVSL.getRowsData(staffListSheet);
+  userRecord = staffList.filter(function(e){
+    return e.workContactWorkEmail == username;
+  })[0];
+  
+  if(userRecord){
+    switch(userRecord.payrollCompanyName){
+      case "NVCHS For Adv Math&Science":
+        userSchool = 'AMS';
+        break;
+      case "NVCHS For Adv Math&Science II":
+        userSchool = 'AMS 2';
+        break;
+      case "NVCHS For Adv Math&Science III":
+        userSchool = 'AMS 3';
+        break;
+      case "NVCHS For Adv Math&Science IV":
+        userSchool = 'AMS 4';
+        break;
+      case "NVCHS For The Humanities":
+        userSchool = 'HUM';
+        break;
+      case "NVCHS For The Humanities II":
+        userSchool = 'HUM 2';
+        break;
+      case "NVCHS For The Humanities III":
+        userSchool = 'HUM 3';
+        break;
+      default:
+        break;
+    };
+    
+    class = userRecord.jobClass;
+    name = userRecord.employeeName;
+    query = 'INSERT INTO Users (username, school, job_class, employee_name) VALUES("' + username + '","'
+            + userSchool + '","' + class + '","' + name + '")';
+    NVGAS.insertSqlRecord(dbString, [query])
+    return true;
+  }
+  return false;
+  debugger;
 }
