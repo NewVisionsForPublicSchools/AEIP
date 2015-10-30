b7c0b3a288c3713246d5de18dd0729fdff3c6cb1
coreyjbrown <coreyjbrown@gmail.com>
2015-10-30 15:00:19 -0400

    Add rejection of proposal

--- Program_Data.js
+++ Program_Data.js
@@ -3,7 +3,7 @@ var dbString = PropertiesService.getScriptProperties().getProperty('DBSTRING');
 
 
 function processPrincipalProposalResponse(formObj){
-	var test, queryArray, queue, responseQuery, html;
+	var test, queryArray, queue, justificationQuery, responseQuery, html;
 
 	queryArray = [];
 	
@@ -12,6 +12,12 @@ function processPrincipalProposalResponse(formObj){
 		case 'Yes':
 			createProgramAnnouncement(formObj.programId);
 			sendProgramAnnouncement(formObj.programId);
+			queue = "";
+			break;
+		case 'No':
+			sendRejectionNotification(formObj.programId, formObj.principalProposalJustification);
+			justificationQuery = 'UPDATE Program_Data pd SET pd.principal_proposal_justification = "' + formObj.principalProposalJustification + '" WHERE pd.program_id = "' + formObj.programId + '"';
+			queryArray.push(justificationQuery);
 			queue = "";
 			break;
 		default:
@@ -25,4 +31,26 @@ function processPrincipalProposalResponse(formObj){
 	html = HtmlService.createTemplateFromFile('principal_proposal_response_confirmation');
 	html.program = getProgramInfo(formObj.programId);
 	return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).getContent();
+}
+
+
+
+function sendRejectionNotification(programId, justification){
+	var test;
+
+	program = getProgramInfo(programId);
+	recipientQuery = 'SELECT username FROM Users WHERE (roles LIKE "%P%" OR roles LIKE "%DSO%") AND school = "' + program.school + '"';
+	recipient = NVGAS.getSqlRecords(dbString, recipientQuery).map(function(e){
+		return e.username;
+	}).join();
+	subject = 'Rejected AEIP Proposal | ' + program.program_name + ' | ' + programId;
+	html = HtmlService.createTemplateFromFile('rejected_proposal_email');
+	html.program = program;
+	html.justification = justification;
+	template = html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).getContent();
+	GmailApp.sendEmail(recipient, subject,"",{htmlBody: template});
+	queryArray = [];
+	notificationQuery = 'UPDATE Program_Data SET program_rejection_notification = "' + new Date() + '" WHERE program_id = "' + programId + '"';
+	queryArray.push(notificationQuery);
+	NVGAS.insertSqlRecord(dbString, queryArray);
 }
